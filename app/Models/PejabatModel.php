80f06b64081f5bde1da887394c6754e1321be45d
Boedy Irhadtanto <boedyirh@yahoo.co.id>
2022-06-13 09:43:41 +0700

    Update PejabatModel.php

--- app/Models/PejabatModel.php
+++ app/Models/PejabatModel.php
@@ -14,17 +14,11 @@ class PejabatModel extends Model
 
     public function updatePejabat($data,$rand_ak1)
     {
-        // DB::table('tbl_pejabat')
-        // ->where('rand_ak1',$rand_ak1)
-        // ->update($data);
-
-        PejabatModel::where('rand_ak1',$rand_ak1)->update($data);
+      PejabatModel::where('rand_ak1',$rand_ak1)->update($data);
     }
 
     public function detailPejabat($pejabat_id)
     {
-        // return DB::table('tbl_pejabat')->where('id', $pejabat_id)
-        // ->get();
-        return PejabatModel::find($pejabat_id)->get();
+       return PejabatModel::find($pejabat_id)->get();
     }
 }
